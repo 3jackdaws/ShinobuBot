c9848abcd6b4ff48bdfbcfa58c430bbc4eb7c95d
Ian Murphy <3jackdaws@gmail.com>
2016-09-24 19:50:50 -0700

    Cleanup

--- MessageLog.py
+++
@@ -1,9 +0,0 @@
-import discord
-
-async def accept_message(message:discord.Message):
-    print("------" + message.author.name + "------", "\n" + message.content + "\n")
-
-def accept_shinobu_instance(i):
-    pass
-
-version = "1.0.1"
