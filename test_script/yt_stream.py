e83263424b3afa9b3a591c790a47fc3e2121e0d0
Ian Murphy <3jackdaws@gmail.com>
2016-09-26 18:24:30 -0700

    Re

--- test_script/yt_stream.py
+++
@@ -1,9 +0,0 @@
-import pafy
-
-video = pafy.new("https://www.youtube.com/watch?v=W_rC-495Z_A")
-audiostream = video.getbestaudio()
-filename = audiostream.download(quiet=True)
-
-print(filename)
-
-
