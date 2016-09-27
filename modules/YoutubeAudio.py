2143e6a2e4dde81ecdebc10676963fde0a1c4be6
Ian Murphy <3jackdaws@gmail.com>
2016-09-26 20:53:38 -0700

    added leave command

--- modules/YoutubeAudio.py
+++ modules/YoutubeAudio.py
@@ -14,16 +14,26 @@ def accept_shinobu_instance(instance):
     shinobu = instance
 
 shinobu = None
+voice_client = None
+
 
 def register_commands(ShinobuCommand):
     @ShinobuCommand("Tells Shinobu to queue a Youtube video")
     async def yt(message:discord.Message, arguments:str):
         print(arguments)
+        global voice_client
         for vchannel in shinobu.get_all_channels():
             if message.author in vchannel.voice_members:
                 if not discord.opus.is_loaded():
                     discord.opus.load_opus("libopus0")
-                shinobuplayer = await shinobu.join_voice_channel(vchannel)
-                youtubeplayer = await shinobuplayer.create_ytdl_player(arguments, use_avconv=True)
+                voice_client = await shinobu.join_voice_channel(vchannel)
+                youtubeplayer = await voice_client.create_ytdl_player(arguments, use_avconv=True)
                 youtubeplayer.start()
 
+    @ShinobuCommand("Tells Shinobu to queue a Youtube video")
+    async def leave(message: discord.Message, arguments: str):
+        global voice_client
+        if arguments == "this channel":
+            channel = discord.utils.find(lambda channel: channel.voice_members == message.author, shinobu.get_all_channels())
+            if channel:
+                await voice_client.disconnect()
