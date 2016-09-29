c4a25047a2d1bac40ebbf40bffc7fee02e9e171e
Ian Murphy <3jackdaws@gmail.com>
2016-09-28 19:07:58 -0700

    Completed dockerfile

--- start_shinobu.sh
+++ start_shinobu.sh
@@ -5,8 +5,10 @@ if [ -e "/etc/shinobu/Shinobu.py" ];then
 	cd /etc/shinobu
 	python Shinobu.py
 else
-	git clone http://github.com/3jackdaws/ShinobuBot shinobu
+	git clone http://github.com/3jackdaws/ShinobuBot
+	mv ShinobuBot/* shinobu/
 	cd shinobu
 	rm start_shinobu.sh
+	mkdir resources
 	python Shinobu.py
 fi
