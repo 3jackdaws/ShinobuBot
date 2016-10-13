dc0d2895542ab38010b6eea5915551af03cba006
Ian Murphy <3jackdaws@gmail.com>
2016-10-13 10:04:26 -0700

    Test gitnotify mod

--- modules/GithubNotifications.py
+++ modules/GithubNotifications.py
@@ -2,14 +2,19 @@ import discord
 import threading
 import asyncio
 
-def send_message():
-    pass
 
-def write_reminder():
-    global reminder_list
+def write_config():
+    global channels
     import json
-    reminder_file = open("resources/reminder_list.json", "w")
-    json.dump(reminder_list, reminder_file)
+    cfile = open("resources/reminder_list.json", "w")
+    json.dump(channels, cfile)
+
+def load_config():
+    global channels
+
+def send_message(message):
+    channel = shinobu.get_channel(232224215175004160)
+    asyncio.ensure_future(shinobu.send_message(channel, message))
 
 def accept_shinobu_instance(i: discord.Client):
     global shinobu, server_thread
@@ -22,16 +27,20 @@ def accept_shinobu_instance(i: discord.Client):
                 import json
                 print("Github endpoint accessed")
                 data = module.request.get_json()
-                for key in data:
-                    print(key)
-                print(data["sender"]["login"])
-                print(data["organization"]["login"])
+                action = data["action"]
+                if action in ["assigned", "unassigned", "labeled", "unlabeled", "opened", "edited", "closed", "reopened"]:
+                    sender = data["sender"]["login"]
+                    org = data["organization"]["login"]
+                    repofull = data["pull_request"]["repo"]["fullname"]
+                    message = "{0} has {1} a pull request in {2}".format(sender, action, repofull)
+                    send_message(message)
                 return "Shinobu Github Endpoint"
             return
     raise ImportWarning("ShinobuEndpointService must be present for the GithubNotifications module to function")
 
 version = "0.0.2"
 shinobu = None # type: discord.Client
+channels = []
 
 
 
