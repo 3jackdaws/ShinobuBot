05db61e5be8c562dc6a285ca38c460db82de4517
Ian Murphy <3jackdaws@gmail.com>
2016-10-13 13:14:06 -0700

    First release of Github Notification module

--- modules/GithubNotifications.py
+++ modules/GithubNotifications.py
@@ -25,24 +25,25 @@ def accept_shinobu_instance(i: discord.Client):
                 import json
                 print("Github endpoint accessed")
                 data = module.request.get_json()
-                action = data["action"]
-                if action in ["assigned", "unassigned", "labeled", "unlabeled", "opened", "edited", "closed", "reopened"]:
-                    sender = data["sender"]["login"]
-                    org = data["organization"]["login"]
-                    repofull = data["repository"]["full_name"]
-                    url = data["pull_request"]["html_url"]
-                    message = "**{0}** has {1} a pull request in {2}\n{3}".format(sender, action, repofull, url)
-                    schannel = None
-                    for channel in shinobu.get_all_channels():
-                        if "neon-aesthetic" in channel.name:
-                            schannel = channel
-                            break
-                    shinobu.quick_send(schannel, message)
+                if "action" in data:
+                    action = data["action"]
+                    if action in ["assigned", "opened", "edited", "closed", "reopened"]:
+                        sender = data["sender"]["login"]
+                        org = data["organization"]["login"]
+                        repofull = data["repository"]["full_name"]
+                        url = data["pull_request"]["html_url"]
+                        message = "**{0}** has {1} a pull request in {2}\n{3}".format(sender, action, repofull, url)
+                        schannel = None
+                        for channel in shinobu.get_all_channels():
+                            if "neon-aesthetic" in channel.name:
+                                schannel = channel
+                                break
+                        shinobu.quick_send(schannel, message)
                 return "Shinobu Github Endpoint"
             return
     raise ImportWarning("ShinobuEndpointService must be present for the GithubNotifications module to function")
 
-version = "0.0.2"
+version = "1.2.0"
 shinobu = None # type: discord.Client
 channels = []
 
@@ -57,7 +58,7 @@ def register_commands(ShinobuCommand):
             print(channel_id)
             org = arguments.rsplit(" ")[2]
             print(org)
-            
+
 
 
 
