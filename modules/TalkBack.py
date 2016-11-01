3ad2381fdd8a6e0e61dfc1b71df0b440bd6b8617
Ian Murphy <3jackdaws@gmail.com>
2016-11-01 11:45:10 -0700

    Redo talkback mod
    Former-commit-id: ce697b9237b22fcfe8553825c33dd5d336cc0083

--- modules/TalkBack.py
+++ modules/TalkBack.py
@@ -1,13 +1,23 @@
 import discord
-from cleverbot import Cleverbot
+from urllib.request import urlopen, Request
 import re
+import json
+
+def get_response(phrase):
+    params = params = json.dumps({"text": phrase}).encode('utf8')
+
+    req = Request("http://127.0.0.1:5001", data=params, headers={'content-type': 'application/json'})
+    return json.loads(urlopen(req).read().decode("utf-8"))["response"]
+
 async def accept_message(message:discord.Message):
     global last_to_me, last_was_question
     if shinobu.user in message.mentions or "shinobu" in message.content.lower() or last_was_question or (last_to_me and "you" in message.content):
         last_to_me = True
-        phrase = re.sub("<@227710246334758913>", "you", message.content)
+        phrase = re.sub("<@227710246334758913>", "you", message.content.lower())
+        # phrase = re.sub("shinobu", "you", message.content)
         print(phrase)
-        response = cbot.ask(phrase)
+        response = get_response(phrase)
+        print(response)
         if "?" in response:
             last_was_question = True
         else:
@@ -26,4 +36,3 @@ last_to_me = False
 last_was_question = False
 version = "1.0.0"
 shinobu = None
-cbot = Cleverbot()
