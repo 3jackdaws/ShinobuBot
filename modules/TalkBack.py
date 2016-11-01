8d20c786bb99357c6916b7b2854f0ca1dd3dbecf
Ian Murphy <3jackdaws@gmail.com>
2016-11-01 10:14:01 -0700

    Update TalkBack mod
    Former-commit-id: 42ded120c2a01640dba765b4cb6bf7598f7ccbbe

--- modules/TalkBack.py
+++ modules/TalkBack.py
@@ -2,14 +2,20 @@ import discord
 from cleverbot import Cleverbot
 import re
 async def accept_message(message:discord.Message):
-    global last_to_me
-    if shinobu.user in message.mentions or "shinobu" in message.content.lower() or (last_to_me and "you" in message.content):
+    global last_to_me, last_was_question
+    if shinobu.user in message.mentions or "shinobu" in message.content.lower() or last_was_question or (last_to_me and "you" in message.content):
         last_to_me = True
         phrase = re.sub("<@227710246334758913>", "you", message.content)
         print(phrase)
-        await shinobu.send_message(message.channel,cbot.ask(phrase))
+        response = cbot.ask(phrase)
+        if "?" in response:
+            last_was_question = True
+        else:
+            last_was_question = False
+        await shinobu.send_message(message.channel,)
     else:
         last_to_me = False
+        last_was_question = False
 
 
 def accept_shinobu_instance(i: discord.Client):
@@ -17,6 +23,7 @@ def accept_shinobu_instance(i: discord.Client):
     shinobu = i
 
 last_to_me = False
+last_was_question = False
 version = "1.0.0"
 shinobu = None
 cbot = Cleverbot()
